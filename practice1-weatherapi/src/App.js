d97e93e9c2cb8e5f07b2e47616843bab54a1ad26
Jack <jackiemiao1121@gmail.com>
2023-09-14 22:50:10 +1000

    weatherapi second commit

--- practice1-weatherapi/src/App.js
+++ practice1-weatherapi/src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useEffect, useState } from "react";
+import axios from "axios";
+const init = [
+  {
+    name: "Boots",
+    number: 2,
+    id: 1,
+  },
+  {
+    name: "Shoes",
+    number: 3,
+    id: 2,
+  },
+  {
+    name: "socks",
+    number: 5,
+    id: 3,
+  },
+];
+export default function App() {
+  const [items, setItems] = useState(init);
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Weather />
+      <PackingList items={items} onHandleAddItem={handleAddItem} />
+      <AddItem onHandleAddItem={handleAddItem} />
     </div>
   );
 }
 
-export default App;
+function Weather() {
+  const [weather, setWeather] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "http://api.weatherapi.com/v1/current.json?key=f1ae9669b74a49fdb42213857231109&q=Sydney"
+      )
+      .then(function (response) {
+        // handle success
+        console.log("response", response.data);
+        setWeather(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("error", error);
+      });
+  }, []);
+  return (
+    <div className="weather">
+      {weather && (
+        <>
+          {weather.location.name}'s current temperature is{" "}
+          {weather.current.temp_c} degree,{" "}
+          {weather.current.temp_c > 10
+            ? "not suitable for skiing"
+            : "good for skiing"}{" "}
+          🌡️
+        </>
+      )}
+    </div>
+  );
+}
+function PackingList({ items, onHandleAddItem }) {
+  return (
+    <div>
+      <h2 className="title">What we need for skiing⛷️</h2>
+      <div className="items">
+        {items.map((item) => (
+          <PackingItem
+            item={item}
+            key={item.id}
+            onHandleAddItem={onHandleAddItem}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+function PackingItem({ item }) {
+  return (
+    <div className="item">
+      {item.number} {item.name}
+    </div>
+  );
+}
+function AddItem({ onHandleAddItem }) {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState(0);
+  // const[id]
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name || !number || number > 10) return;
+    const id = crypto.randomUUID();
+    const newItem = { name, number, id: { id } };
+    onHandleAddItem(newItem);
+  }
+  return (
+    <div>
+      <form className="add-item-form">
+        <div className="add-item">
+          <label>What else do we need?🟰</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label>How many do we need?🎿</label>
+
+          <input
+            type="text"
+            value={number}
+            onChange={(e) => setNumber(Number(e.target.value))}
+          />
+        </div>
+        <button onClick={handleSubmit}>Add</button>
+      </form>
+    </div>
+  );
+}
